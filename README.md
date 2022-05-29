eb0c873c03882167af66d8602ba449fb56f8162c
pagesMp <marc.msp97@gmail.com>
2022-05-30 00:08:09 +0200

    Readme y repaso codigos

---
+++ README.md
@@ -0,0 +1,15 @@
+## PROYECTO 3 
+---
+
+En este proyecto, he realizado un videojuego en HTML, CSS y JavaScript.
+
+La tematica del juego, es de lucha de un jugador (jugador/maquina) en el qual, el player,
+elige un luchador para el y el otro sera para la máquina y se hara un combate aleatorio.
+
+---
+
+## lenguajes 
+
+HTML CSS Y JS
+
+ 
--- js/main.js
+++ js/main.js
@@ -4,34 +4,24 @@
 
 //         res[i].hidden= true;
 
-
 //     }
 // document.querySelector(".container"+view).hidden=false;
 
-// }
-
-// changeView(2);
-
-//cambio de sections en el html
-
-
 const saltarPantalla = (change) => {
 
-    let elegirPantalla = change; //pantallas2
+    let elegirPantalla = change; 
 
     let arrayScreens = ["pantallaInicial", "pantallaSelecion","pantallaLucha","pantallaVictoria"];
 
-    //Le decimos a filter que si encuentra el valor que le pasamos en el array, no lo incluya
     arrayScreens = arrayScreens.filter(val => !elegirPantalla.includes(val));
-    //Le ponemos display "block" a la pantalla deseada
+    
     document.getElementById(elegirPantalla).style.display = "block";
-    //Recorremos el arrayPantallas para poner el display:none
+   
     for (let pantallas of arrayScreens) {
         document.getElementById(pantallas).style.display = "none";
     }
 
 }
-
 
 const selectFruit = (fruit) => { 
 
@@ -46,7 +36,7 @@ const selectFruit = (fruit) => {
 
 function obtenerNumeroRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-  }
+}
 
 
 class fruta {
